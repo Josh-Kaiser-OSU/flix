786e773bfcd596411cfb888f2b029a30c8047932
Josh-Kaiser-OSU <kaiserjo@oregonstate.edu>
2021-02-21 11:38:12 -0500

    Initial Commit

---
+++ flixster/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  flixster
+//
+//  Created by Joshua Kaiser on 2/17/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
